e3d7976c1f6834c1f0cb34fec2895d64aa558c05
Ivan Baidakou <basiliscos@openmailbox.org>
2017-02-18 22:15:05 +0300

    Format, be win32-friendly

--- EmptyPort.hpp
+++ EmptyPort.hpp
@@ -1,16 +1,19 @@
 #pragma once
 /* platform-specific headers */
 
-#ifdef _WIN32
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
-    #define CLOSE_SOCKET(s) closesocket(s)
+#if defined(_WIN32) || defined(WIN32)
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#define SOCKET_TYPE SOCKET
+#define CLOSE_SOCKET(s) closesocket(s)
+#pragma comment(lib, "Ws2_32.lib")
 #else
-    #include <sys/socket.h>
-    #include <arpa/inet.h>
-    #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
-    #include <unistd.h> /* Needed for close() */
-    #define CLOSE_SOCKET(s) close(s)
+#define SOCKET_TYPE int
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
+#include <unistd.h> /* Needed for close() */
+#define CLOSE_SOCKET(s) close(s)
 #endif
 
 #include <cstring>
@@ -22,107 +25,110 @@
 
 namespace empty_port {
 
-    enum Kind {
-        TCP,
-        UDP
-    };
+enum Kind { TCP, UDP };
 
-    using socket_t = int;
-    using port_t = uint16_t;
-    using duration_t = std::chrono::milliseconds;
-    static constexpr duration_t SLEEP_DELAY(10);
+using socket_t = SOCKET_TYPE;
+using port_t = uint16_t;
+using duration_t = std::chrono::milliseconds;
+using clock_t = std::chrono::high_resolution_clock;
+static constexpr duration_t SLEEP_DELAY(10);
 
-    static constexpr port_t MIN_PORT = 49152;
-    static constexpr port_t MAX_PORT = 65535;
+static constexpr port_t MIN_PORT = 49152;
+static constexpr port_t MAX_PORT = 65535;
 
-    template<Kind>
-    struct impl;
+template <Kind> struct impl;
 
-    template<>
-    struct impl<TCP>{
+template <> struct impl<TCP> {
 
-        static bool check_port_impl(const port_t port, const char* host) {
-            socket_t s = socket(AF_INET, SOCK_STREAM, 0);
-            if (s == -1) {
-                return false;
-            }
-            sockaddr_in remote_addr;
-            std::memset(&remote_addr, 0, sizeof(remote_addr));
-            remote_addr.sin_family = AF_INET;
-            remote_addr.sin_port = htons(port);
+    static bool check_port_impl(const port_t port, const char *host);
 
-            struct addrinfo hints, *result = NULL;
-            std::memset(&hints, 0, sizeof(hints));
-            hints.ai_family = AF_INET;
-            hints.ai_socktype = SOCK_STREAM;
+    static port_t get_random_impl(const char *host);
 
-            struct sockaddr_in* host_addr;
+    static bool wait_port_impl(const port_t port, const char *host,
+                               duration_t max_wait_ms);
+};
 
-            // looks up IPv4/IPv6 address by host name or stringized IP address
-            if ( getaddrinfo(host, NULL, &hints, &result) ) {
-                goto FAIL;
-            }
-            host_addr = (struct sockaddr_in*)result->ai_addr;
-            memcpy(&remote_addr.sin_addr, &host_addr->sin_addr, sizeof(struct in_addr));
-            freeaddrinfo(result);
-
-            if((connect(s, (struct sockaddr *)&remote_addr, sizeof(remote_addr))) < 0) {
-                CLOSE_SOCKET(s);
-                return true;
-            } else {
-                goto FAIL;
-            }
-
-            FAIL:
-            CLOSE_SOCKET(s);
-            return false;
+/* TCP impl */
+inline bool impl<TCP>::wait_port_impl(const port_t port, const char *host,
+                                      duration_t max_wait_ms) {
+    auto stop_at = clock_t::now() + max_wait_ms;
+    do {
+        if (!impl<TCP>::check_port_impl(port, host)) {
+            return true;
         }
+        std::this_thread::sleep_for(SLEEP_DELAY);
+    } while (clock_t::now() < stop_at);
 
-        static port_t get_random_impl(const char* host) {
-            std::random_device rd;
-            std::mt19937 rng(rd());
-            std::uniform_int_distribution<port_t> uni(MIN_PORT, MAX_PORT);
+    return false;
+}
 
-            port_t some_port = uni(rng);
-            while (some_port < MAX_PORT) {
-                if (check_port_impl(some_port, host)) {
-                    return some_port;
-                }
-                some_port++;
-            }
-            throw std::runtime_error("Cannot get random port");
+inline port_t impl<TCP>::get_random_impl(const char *host) {
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<port_t> uni(MIN_PORT, MAX_PORT);
+
+    port_t some_port = uni(rng);
+    while (some_port < MAX_PORT) {
+        if (impl<TCP>::check_port_impl(some_port, host)) {
+            return some_port;
         }
+        some_port++;
+    }
+    throw std::runtime_error("Cannot get random port");
+}
 
-        static bool wait_port_impl(const port_t port, const char* host, duration_t max_wait_ms) {
-            duration_t waited(0);
-            while( waited < max_wait_ms) {
-                if (!check_port_impl(port, host)) {
-                    return true;
-                }
-                std::this_thread::sleep_for(SLEEP_DELAY);
-                waited += SLEEP_DELAY;
-            }
+inline bool impl<TCP>::check_port_impl(const port_t port, const char *host) {
+    socket_t s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s == -1) {
+        throw std::runtime_error("Cannot get random port:: socket");
+        return false;
+    }
+    sockaddr_in remote_addr;
+    std::memset(&remote_addr, 0, sizeof(remote_addr));
+    remote_addr.sin_family = AF_INET;
+    remote_addr.sin_port = htons(port);
 
-            return false;
-        }
-    };
+    struct addrinfo hints, *result = NULL;
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
 
-    template<Kind T>
-    bool check_port(const port_t port, const char* host = "127.0.0.1") {
-        return impl<T>::check_port_impl(port, host);
+    struct sockaddr_in *host_addr;
+
+    // looks up IPv4/IPv6 address by host name or stringized IP address
+    if (getaddrinfo(host, NULL, &hints, &result)) {
+        throw std::runtime_error("Cannot get random port:: getaddrinfo");
+        goto FAIL;
+    }
+    host_addr = (struct sockaddr_in *)result->ai_addr;
+    memcpy(&remote_addr.sin_addr, &host_addr->sin_addr, sizeof(struct in_addr));
+    freeaddrinfo(result);
+
+    if ((connect(s, (struct sockaddr *)&remote_addr, sizeof(remote_addr)))) {
+        CLOSE_SOCKET(s);
+        return true;
+    } else {
+        goto FAIL;
     }
 
-    template<Kind T>
-    port_t get_random(const char* host = "127.0.0.1") {
-        return impl<T>::get_random_impl(host);
-    }
+FAIL:
+    CLOSE_SOCKET(s);
+    return false;
+}
 
-    template<Kind T>
-    bool wait_port(const port_t port, const char* host = "127.0.0.1",
-        duration_t max_wait_ms = duration_t(500))
-    {
-        return impl<T>::wait_port_impl(port, host, max_wait_ms);
-    }
+/* public interface */
+template <Kind T>
+inline bool check_port(const port_t port, const char *host = "127.0.0.1") {
+    return impl<T>::check_port_impl(port, host);
+}
 
+template <Kind T> inline port_t get_random(const char *host = "127.0.0.1") {
+    return impl<T>::get_random_impl(host);
+}
 
+template <Kind T>
+inline bool wait_port(const port_t port, const char *host = "127.0.0.1",
+                      duration_t max_wait_ms = duration_t(500)) {
+    return impl<T>::wait_port_impl(port, host, max_wait_ms);
+}
 };
--- t/10-tcp.cpp
+++ t/10-tcp.cpp
@@ -1,18 +1,31 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
 
 #include <EmptyPort.hpp>
+#include "catch.hpp"
 
 namespace ep = empty_port;
 constexpr ep::Kind TCP = ep::Kind::TCP;
 
-TEST_CASE( "single port", "[tcp]" ) {
+static bool initialised = false;
 
+static void initialise() {
+    if (initialised == false) {
+#if defined(_WIN32) || defined(WIN32)
+        WSADATA data = {0};
+        if (WSAStartup(MAKEWORD(2, 2), &data) != 0) {
+            throw std::runtime_error("WSAStartup failed");
+        }
+#endif
+        initialised = true;
+    }
+}
+
+TEST_CASE("single port", "[tcp]") {
+    initialise();
     auto port = ep::get_random<TCP>();
-    REQUIRE (port > 0);
-    REQUIRE (ep::check_port<TCP>(port));
-    REQUIRE (!ep::wait_port<TCP>(port));
-
+    REQUIRE(port > 0);
+    REQUIRE(ep::check_port<TCP>(port));
+    REQUIRE(!ep::wait_port<TCP>(port));
 
     auto s = socket(AF_INET, SOCK_STREAM, 0);
     if (s < 0) {
@@ -25,27 +38,26 @@ TEST_CASE( "single port", "[tcp]" ) {
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = INADDR_ANY;
-    if (bind(s, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         perror("ERROR on binding");
         exit(1);
     }
     listen(s, 5);
 
-    REQUIRE( !ep::check_port<TCP>(port) );
-    REQUIRE (ep::wait_port<TCP>(port));
-
+    REQUIRE(!ep::check_port<TCP>(port));
+    REQUIRE(ep::wait_port<TCP>(port));
 }
 
-TEST_CASE( "10 random empty ports", "[tcp]" ) {
+TEST_CASE("10 random empty ports", "[tcp]") {
     static constexpr size_t COUNT = 10;
     ep::port_t ports[COUNT] = {0};
-    for (auto i =0; i < COUNT; i++) {
+    for (auto i = 0; i < COUNT; i++) {
         ports[i] = ep::get_random<TCP>();
         REQUIRE(ports[i] > 0);
     }
     // check for uniqueness
-    for (auto i =0; i < COUNT; i++) {
-        for (auto j = i+1; j < COUNT; j++) {
+    for (auto i = 0; i < COUNT; i++) {
+        for (auto j = i + 1; j < COUNT; j++) {
             REQUIRE(ports[i] != ports[j]);
         }
     }
